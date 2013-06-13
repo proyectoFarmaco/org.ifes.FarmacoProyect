fec1fe45c6ff5c9ad3067d3d2cd4fda020f2c164
matias nahuel heredia <matiasnahuelheredia@gmail.com>
2013-06-13 19:20:12 -0300

    agregacion de clases email y telefono

---
+++ dom/src/main/java/dom/farmacia/CorreoElectronico.java
@@ -0,0 +1,20 @@
+package dom.farmacia;
+
+public class CorreoElectronico {
+
+	private String descripcion;
+	private String direccion;
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
---
+++ dom/src/main/java/dom/farmacia/Telefono.java
@@ -0,0 +1,19 @@
+package dom.farmacia;
+
+public class Telefono {
+
+	private String descripcion;
+	private String numTelefono;
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getNumTelefono() {
+		return numTelefono;
+	}
+	public void setNumTelefono(String numTelefono) {
+		this.numTelefono = numTelefono;
+	}
+}
